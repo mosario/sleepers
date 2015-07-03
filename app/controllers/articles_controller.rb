d430fddda276d035b94d44c0e720814dcf99a2ad
mosario <malysh_cymo@mail.ru>
2015-07-03 15:44:52 +0400

    breadcrumbs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,20 +2,32 @@ class ArticlesController < ApplicationController
 	
 	http_basic_authenticate_with name: "demo", password: "demo", except: [:index, :show]
 
+	add_breadcrumb "Главная", :root_path, :options => { :title => "Назад" }
+
 	def index
 		@articles = Article.all
+		add_breadcrumb "Список", @articles_path
 	end
 
 	def show
 		@article = Article.find(params[:id])
+
+		add_breadcrumb "Список", articles_path
+		add_breadcrumb "Детальная", @articles_path
 	end
 
 	def new
 		@article = Article.new
+
+		add_breadcrumb "Список", articles_path
+		add_breadcrumb "Создать", @articles_path
 	end
 
 	def edit
 		@article = Article.find(params[:id])
+
+		add_breadcrumb "Список", articles_path
+		add_breadcrumb "Редактирование", @articles_path
 	end
 
 	def create
